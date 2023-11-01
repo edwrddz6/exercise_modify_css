d602362d839fc416f1e07d6d475f38817328dfc4
edwrddz6 <134966646+edwrddz6@users.noreply.github.com>
2023-11-01 10:10:20 -0700

    Add DOM interactions for examples

---
+++ script.js
@@ -0,0 +1,34 @@
+ // Example 1
+
+let backgroundColor = document.getElementById("example2");
+backgroundColor.style.backgroundColor = "#FFFF00";
+
+// Example 2
+
+let fontSize = document.getElementById("example2");
+fontSize.style.fontSize = "24px";
+
+// Example 3
+
+let colorHead = document.querySelector("h2");
+colorHead.style.color = "red";
+
+// Example 4
+
+let fontColor = document.querySelector("button");
+fontColor.style.border = "1px solid black";
+
+// Example 5
+
+let newImg = document.querySelector("section");
+newImg.style.backgroundImage = "url('https://images.unsplash.com/photo-1698444214003-dfdea976064a?auto=format&fit=crop&q=80&w=2062&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')";
+
+// Example 6
+
+let imgDisappear = document.getElementById('example6');
+imgDisappear.style.display = "none";
+
+// Example 7
+
+let fontColorBaby = document.querySelector('.example7');
+fontColorBaby.style.color = "green";
